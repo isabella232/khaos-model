4f403a94f95ec0754c0bf4c15e30e6c29f44598e
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-08-12 08:14:14 -0400

    default props

--- template/index.js
+++ template/index.js
@@ -3,7 +3,10 @@ var AmpersandCollection = require('ampersand-rest-collection');
 
 var {{titlecase name}} = AmpersandModel.extend({
   modelType: '{{titlecase name}}',
-  idAttribute: '_id'
+  idAttribute: '_id',
+  props: {
+    _id: 'string'
+  },
 });
 
 var {{titlecase name}}Collection = AmpersandCollection.extend({
