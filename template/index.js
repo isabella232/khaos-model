c93d10e1af0e1aebe89b4b0c697e0372fc4e9b23
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-08-12 08:12:02 -0400

    trailing comma

--- template/index.js
+++ template/index.js
@@ -3,12 +3,13 @@ var AmpersandCollection = require('ampersand-rest-collection');
 
 var {{titlecase name}} = AmpersandModel.extend({
   modelType: '{{titlecase name}}',
-  idAttribute: '_id',
+  idAttribute: '_id'
 });
 
 var {{titlecase name}}Collection = AmpersandCollection.extend({
   comparator: '_id',
-  model: {{titlecase name}}
+  model: {{titlecase name}},
+  modelType: '{{titlecase name}}Collection',
 });
 
 module.exports = {{titlecase name}};
